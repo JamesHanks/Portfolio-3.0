436b084f32a36081c36276109af0b391caedfc77
JamesHanks <james.ra.hanks@gmail.com>
2018-05-07 21:08:58 +0100

    Revert

--- inc/footer.php
+++
@@ -1,5 +0,0 @@
-		<footer>
-			<small>© James Hanks <?php echo date("Y");?></small>
-		</footer>
-	</body>
-</html>
--- index.php
+++ index.php
@@ -1,7 +1,64 @@
-<?php include "inc/header.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>James Hanks</title>
+		<meta name="author" content="James Hanks">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="distribution" content="Global">
+		<meta name="language" content="English">
+		<meta name="robot" content="index,follow">
+		<meta name="url" content="www.jameshanks.net">
+
+		<!-- OG Tags -->
+
+		<!-- Twitter Tags -->
+
+		<!-- Icons -->
+
+		<!-- Stylesheets -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karla">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/styles.css">
+
+        <!-- Scripts -->
+
+    </head>
+	<body>
+		<header>
+			<div>
+	    		<img src="images/logo.svg" alt="🐙">
+	    		<img src="images/james-hanks.svg" alt="James Hanks">
+			</div>
+			<nav>
+				<a href="#">About</a>
+			</nav>
+		</header>
 
 		<section>
-			<p>Let the games begin.</p>
+			<div>
+				<div>
+					<h1>Project Name</h1>
+					<p>This is an example project description.</p>
+					<small>Tag 1, Tag 2, Tag 3</small>
+				</div>
+				<img src="images/project.jpg" alt="💔">
+			</div>
 		</section>
 
-<?php include "inc/footer.php" ?>
+		<footer>
+			<small>© James Hanks <?php echo date("Y");?></small>
+			<ul>
+				<li><a href="#"><img src="" alt="Twitter"></a></li>
+				<li><a href="#"><img src="" alt="Instagram"></a></li>
+				<li><a href="#"><img src="" alt="LinkedIn"></a></li>
+				<li><a href="#"><img src="" alt="GitHub"></a></li>
+				<li><a href="#"><img src="" alt="Behance"></a></li>
+			</ul>
+		</footer>
+	</body>
+</html>
