bd3a29066e64a57d380de289b31eff7bd9a083fd
JamesHanks <james.ra.hanks@gmail.com>
2018-05-10 11:52:52 +0100

    Reformatted

--- index.php
+++ index.php
@@ -1,64 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title>James Hanks</title>
-		<meta name="author" content="James Hanks">
-		<meta name="description" content="">
-		<meta name="keywords" content="">
-		<meta name="distribution" content="Global">
-		<meta name="language" content="English">
-		<meta name="robot" content="index,follow">
-		<meta name="url" content="www.jameshanks.net">
+	<!-- Meta Info -->
+	<title>James Hanks</title>
+	<meta name="author" content="James Hanks">
+	<meta name="description" content="Designer & Developer">
+	<meta name="keywords" content="designer, developer, web, interface, interaction, experience, UI, UX, front-end, JavaScript">
+	<meta name="distribution" content="Global">
+	<meta name="language" content="English">
+	<meta name="robot" content="index,follow">
+	<meta name="url" content="www.jameshanks.net">
+	<meta name="google-site-verification" content="googlea91780bc9e6ce521.html">
 
-		<!-- OG Tags -->
+	<!-- Sharing -->
+	<meta itemprop="name" content="James Hanks">
+	<meta itemprop="description" content="Designer & Developer">
+	<meta itemprop="image" content="images/image.png">
 
-		<!-- Twitter Tags -->
+	<meta name="twitter:title" content="James Hanks">
+	<meta name="twitter:description" content="Designer & Developer">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@jameshanks_">
+	<meta name="twitter:creator" content="@jameshanks_">
+	<meta name="twitter:image" content="images/image.png">
 
-		<!-- Icons -->
+	<meta property="og:title" content="James Hanks">
+	<meta property="og:description" content="Designer & Developer">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://jameshanks.net">
+	<meta property="og:image" content="images/image.png">
 
-		<!-- Stylesheets -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karla">
-        <link rel="stylesheet" href="css/reset.css">
-        <link rel="stylesheet" href="css/styles.css">
+	<!-- Icons -->
+	<link rel="icon" href="images/logo.svg">
+	<link rel="apple-touch-icon" href="images/touch-icon.png">
 
-        <!-- Scripts -->
+	<!-- Styles -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karla">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/styles.css">
 
-    </head>
-	<body>
-		<header>
+	<!-- Scripts -->
+
+</head>
+<body>
+	<header>
+		<div>
+			<img src="images/logo.svg" alt="🐙">
+			<img src="images/james-hanks.svg" alt="James Hanks">
+		</div>
+		<nav>
+			<a href="#">About</a>
+		</nav>
+	</header>
+
+	<section>
+		<div>
 			<div>
-	    		<img src="images/logo.svg" alt="🐙">
-	    		<img src="images/james-hanks.svg" alt="James Hanks">
+				<h1>Project Name</h1>
+				<p>This is an example project description.</p>
+				<small>Tag 1, Tag 2, Tag 3</small>
 			</div>
-			<nav>
-				<a href="#">About</a>
-			</nav>
-		</header>
+			<img src="images/project.jpg" alt="💔">
+		</div>
+	</section>
 
-		<section>
-			<div>
-				<div>
-					<h1>Project Name</h1>
-					<p>This is an example project description.</p>
-					<small>Tag 1, Tag 2, Tag 3</small>
-				</div>
-				<img src="images/project.jpg" alt="💔">
-			</div>
-		</section>
-
-		<footer>
-			<small>© James Hanks <?php echo date("Y");?></small>
-			<ul>
-				<li><a href="#"><img src="" alt="Twitter"></a></li>
-				<li><a href="#"><img src="" alt="Instagram"></a></li>
-				<li><a href="#"><img src="" alt="LinkedIn"></a></li>
-				<li><a href="#"><img src="" alt="GitHub"></a></li>
-				<li><a href="#"><img src="" alt="Behance"></a></li>
-			</ul>
-		</footer>
-	</body>
+	<footer>
+		<small>© James Hanks <?php echo date("Y");?></small>
+		<ul>
+			<li><a href="#"><img src="" alt="Twitter"></a></li>
+			<li><a href="#"><img src="" alt="Instagram"></a></li>
+			<li><a href="#"><img src="" alt="LinkedIn"></a></li>
+			<li><a href="#"><img src="" alt="GitHub"></a></li>
+			<li><a href="#"><img src="" alt="Behance"></a></li>
+		</ul>
+	</footer>
+</body>
 </html>
