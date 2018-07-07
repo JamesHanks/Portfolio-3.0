e290fa152b76d1d7e38b7dfb039d3a1354d0947c
JamesHanks <james.ra.hanks@gmail.com>
2018-07-07 01:12:09 +0100

    Layout

--- index.php
+++ index.php
@@ -8,27 +8,28 @@
 	<!-- Meta Info -->
 	<title>James Hanks</title>
 	<meta name="author" content="James Hanks">
-	<meta name="description" content="Designer and developer based in London.">
-	<meta name="keywords" content="designer, design, developer, digital, front-end, interaction, interface, JavaScript, software, UI, UX, web">
+	<meta name="description" content="James Hanks is an interactive designer and developer based in London.">
+	<meta name="keywords" content="designer, design, developer, digital, front-end, interaction, interface, javascript, software, UI, UX, web">
 	<meta name="distribution" content="Global">
 	<meta name="language" content="English">
 	<meta name="robot" content="index,follow">
 	<meta name="url" content="http://www.jameshanks.net">
+	<meta name="theme-color" content="#5452C1">
 
 	<!-- Sharing -->
 	<meta itemprop="name" content="James Hanks">
-	<meta itemprop="description" content="Designer and developer based in London.">
+	<meta itemprop="description" content="James Hanks is an interactive designer and developer based in London">
 	<meta itemprop="image" content="images/thumbnail.png">
 
 	<meta name="twitter:title" content="James Hanks">
-	<meta name="twitter:description" content="Designer and developer based in London.">
+	<meta name="twitter:description" content="James Hanks is an interactive designer and developer based in London">
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:site" content="@jameshanks_">
 	<meta name="twitter:creator" content="@jameshanks_">
 	<meta name="twitter:image" content="images/thumbnail.png">
 
 	<meta property="og:title" content="James Hanks" />
-	<meta property="og:description" content="Designer and developer based in London." />
+	<meta property="og:description" content="James Hanks is an interactive designer and developer based in London" />
 	<meta property="og:url" content="http://www.jameshanks.net/index.php" />
 	<meta property="og:image" content="http://www.jameshanks.net/images/thumbnail.png" />
 	<meta property="og:type" content="website" />
@@ -40,46 +41,121 @@
 
 	<!-- Styles -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karla">
-	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/styles.css">
 
 	<!-- Scripts -->
 
 </head>
 <body>
-	<header>
-		<div>
-			<img src="images/logo.svg" alt="🐙">
-			<img src="images/james-hanks.svg" alt="James Hanks">
-		</div>
-		<nav>
-			<ul>
-				<li><a href="#">About</a></li>
-			</ul>
-		</nav>
-	</header>
 
-	<section>
-		<div>
-			<div>
-				<h1>Project Name</h1>
-				<p>This is an example project description.</p>
-				<small>Tag 1, Tag 2, Tag 3</small>
+	<!-- HEADER -->
+	<header>
+		<div class="header">
+			<div class="header-brand">
+				<a href="#">
+					<svg class="header-brand-logo">
+						<path id="left-eye" d="M24.6,30.6L24.6,30.6c-2.7,0-4.9,2.2-4.9,4.9c0,2.7,2.2,4.9,4.9,4.9h0c2.7,0,4.9-2.2,4.9-4.9C29.5,32.8,27.3,30.6,24.6,30.6z"/>
+						<path id="right-eye" d="M50.4,30.6L50.4,30.6c-2.7,0-4.9,2.2-4.9,4.9c0,2.7,2.2,4.9,4.9,4.9h0c2.7,0,4.9-2.2,4.9-4.9C55.4,32.8,53.2,30.6,50.4,30.6z"/>
+						<path id="body" d="M58.9,54.5h-2.7c-2.8,0-5.1-2-5.7-4.6c-0.1-0.3,0-0.6,0.2-0.9c0.3-0.3,0.6-0.4,1.1-0.4 c6.5-0.7,11.7-6.3,11.7-13c0-14.3-11.6-25.9-25.9-25.9h0c-14.3,0-25.9,11.6-25.9,25.9c0,6.7,5.2,12.3,11.7,13 c0.4,0,0.8,0.1,1.1,0.4c0.2,0.3,0.2,0.6,0.2,0.9c-0.6,2.6-2.9,4.6-5.7,4.6h-2.7c-1.2,0-2.2,0.9-2.3,2c-0.1,1.3,0.9,2.3,2.2,2.3h2.7c5.2,0,9.6-3.9,10.2-9c0-0.8,0.6-1.4,1.4-1.4h0c0.8,0,1.4,0.6,1.4,1.4v14.8c0,1.2,1,2.2,2.2,2.2s2.2-1,2.2-2.2V49.9c0-0.8,0.6-1.4,1.4-1.4h0c0.8,0,1.4,0.6,1.4,1.4v14.8c0,1.2,1,2.2,2.2,2.2s2.2-1,2.2-2.2V49.9c0-0.8,0.6-1.4,1.4-1.4h0c0.8,0,1.4,0.6,1.4,1.4c0.6,5,5,9,10.2,9H59c1.3,0,2.3-1.1,2.2-2.3C61.1,55.4,60.1,54.5,58.9,54.5z M32.6,35.6c0,4.4-3.6,8.1-8.1,8.1h0c-4.4,0-8.1-3.6-8.1-8.1v0c0-4.4,3.6-8.1,8.1-8.1h0C29,27.5,32.6,31.1,32.6,35.6L32.6,35.6z M50.4,43.6c-4.4,0-8.1-3.6-8.1-8.1v0c0-4.4,3.6-8.1,8.1-8.1h0c4.4,0,8.1,3.6,8.1,8.1v0C58.5,40,54.9,43.6,50.4,43.6L50.4,43.6z"/>
+						<path id="ring" d="M37.5,2.2c9.4,0,18.3,3.7,25,10.3c6.7,6.7,10.3,15.5,10.3,25s-3.7,18.3-10.3,25c-6.7,6.7-15.5,10.3-25,10.3c-9.4,0-18.3-3.7-25-10.3c-6.7-6.7-10.3-15.5-10.3-25s3.7-18.3,10.3-25C19.2,5.9,28.1,2.2,37.5,2.2 M37.5,0L37.5,0C16.8,0,0,16.8,0,37.5v0C0,58.2,16.8,75,37.5,75h0C58.2,75,75,58.2,75,37.5v0C75,16.8,58.2,0,37.5,0L37.5,0z"/>
+					</svg>
+				</a>
+				<span class="header-brand-name">
+					<h1>James Hanks</h1>
+					<h2>Designer & Developer</h2>
+				</span>
 			</div>
-			<img src="images/project.jpg" alt="💔">
+			<nav class="header-nav">
+				<span><a href="#">About</a></span>
+				<span>—</span>
+				<span><a href="#">Work</a></span>
+			</nav>
 		</div>
-		
+
+		<!-- INTRO -->
+		<div class="introduction">
+			<p>I’m an interactive designer and developer based in London. I strive to build elegant, user-centred and meaningful digital experiences.<br><br>You can view some examples of my work <a href="#">below</a>.</p>
+		</div>
+
+	</header>
+	<span class="border"></span>
+
+	<!-- CONTENT -->
+	<section class="content">
+
+		<!-- PROJECTS -->
+		<a href="#" class="project-link">
+			<div class="project-container">
+				<img src="images/project-image-small.jpg" class="project-image">
+				<div class="project-overlay">
+					<div class="project-overlay-summary"></div>
+					<div class="project-overlay-colour"></div>
+				</div>
+			</div>
+			<span class="project-details">
+				<h4>Somnus</h4>
+				<h5>Product Design, UI/UX</h5>
+			</span>
+		</a>
+
+		<a href="#" class="project-link">
+			<div class="project-container">
+				<img src="images/project-image-large.jpg" class="project-image">
+				<div class="project-overlay">
+					<div class="project-overlay-summary"></div>
+					<div class="project-overlay-colour"></div>
+				</div>
+			</div>
+			<span class="project-details">
+				<h4>Espia</h4>
+				<h5>Interaction Design, Robotics, UI/UX</h5>
+			</span>
+		</a>
+
+		<a href="#" class="project-link">
+			<div class="project-container">
+				<img src="images/project-image-small.jpg" class="project-image">
+				<div class="project-overlay">
+					<div class="project-overlay-summary"></div>
+					<div class="project-overlay-colour"></div>
+				</div>
+			</div>
+			<span class="project-details">
+				<h4>Apto Hotels</h4>
+				<h5>Branding, Service Design, UI/UX</h5>
+			</span>
+		</a>
+
 	</section>
 
+
+	<!-- FOOTER -->
+	<span class="border"></span>
 	<footer>
-		<small>© James Hanks <?php echo date("Y");?></small>
-		<ul>
-			<li><a href="#"><img src="" alt="Twitter"></a></li>
-			<li><a href="#"><img src="" alt="Instagram"></a></li>
-			<li><a href="#"><img src="" alt="LinkedIn"></a></li>
-			<li><a href="#"><img src="" alt="GitHub"></a></li>
-			<li><a href="#"><img src="" alt="Behance"></a></li>
-		</ul>
+
+		<div class="footer">
+			<div class="footer-copyright">
+				<small>Built by James Hanks © <?php echo date("Y");?></small>
+			</div>
+
+			<div class="footer-social">
+				<span class="footer-social-link">
+					<a href="https://www.behance.net/jameshanks"><svg class="footer-logo"><path d="M17.5,1A16.48,16.48,0,0,1,29.15,29.15,16.48,16.48,0,1,1,5.85,5.85,16.38,16.38,0,0,1,17.5,1m0-1h0A17.51,17.51,0,0,0,0,17.5H0A17.51,17.51,0,0,0,17.5,35h0A17.51,17.51,0,0,0,35,17.5h0A17.51,17.51,0,0,0,17.5,0Z"/><path d="M15.68,17.22a2.35,2.35,0,0,0,2.07-2.58c0-2.43-1.69-3.61-3.84-3.61H6.85V24.6h7.06s4.31.13,4.31-4C18.22,20.59,18.41,17.22,15.68,17.22ZM10,13.44h3.95s1,0,1,1.41-.56,1.62-1.2,1.62H10Zm3.77,8.75H10V18.56h3.95s1.43,0,1.43,1.86C15.34,22,14.3,22.17,13.73,22.19Z"/><path d="M24,14.48c-5.22,0-5.21,5.21-5.21,5.21A4.81,4.81,0,0,0,24,24.88s4.64.26,4.64-3.61H26.22s.08,1.46-2.17,1.46a2.21,2.21,0,0,1-2.39-2.36h7S29.46,14.48,24,14.48Zm-2.34,4.08A2.44,2.44,0,0,1,24,16.47a2,2,0,0,1,2.07,2.09Zm-.7-6.82h5.62v1.68H20.93Z"/></svg></a>
+				</span>
+				<span class="footer-social-link">
+					<a href="https://www.github.com/jameshanks"><svg class="footer-logo"><path d="M17.5,1A16.48,16.48,0,0,1,29.15,29.15,16.48,16.48,0,1,1,5.85,5.85,16.38,16.38,0,0,1,17.5,1m0-1h0A17.51,17.51,0,0,0,0,17.5H0A17.51,17.51,0,0,0,17.5,35h0A17.51,17.51,0,0,0,35,17.5h0A17.51,17.51,0,0,0,17.5,0Z"/> <path d="M11.65,33.18c1.81-1.09,1.65-3.85,1.68-5.46a3.85,3.85,0,0,1-3.15-.29,3.45,3.45,0,0,1-1-.84c-.85-1.07-1.64-2.18-2.5-3.35.2-.31.66-.27,1-.08a4.82,4.82,0,0,1,1.24,1A4.67,4.67,0,0,0,11.52,26a2.28,2.28,0,0,0,2.21-1,4.64,4.64,0,0,1,1.07-1,3.53,3.53,0,0,1,.77-.35c-1-.21-2-.34-2.88-.6-3.59-1-4.93-3.26-4.81-7a6.32,6.32,0,0,1,1.41-3.92,1.07,1.07,0,0,0,.08-.74,6.86,6.86,0,0,1,.36-3.56,7.45,7.45,0,0,1,3.85,1.32,1.08,1.08,0,0,0,.73.13A16.44,16.44,0,0,1,21,9.25a1.49,1.49,0,0,0,.92-.19,7.6,7.6,0,0,1,3.38-1.2c.3,0,.49,0,.53.34.08.89.25,1.77.25,2.65a2.19,2.19,0,0,0,.39,1.48A7.36,7.36,0,0,1,27.63,18c-.44,3.67-3.43,5.13-5.95,5.43l-1.15.16a2.76,2.76,0,0,0,.44.32,3,3,0,0,1,1.59,2.9c0,1.06.05,2.13,0,3.19a8.2,8.2,0,0,0,1.05,3,14,14,0,0,1-6,1.08A20.84,20.84,0,0,1,11.65,33.18Z"/></svg></a>
+				</span>
+				<span class="footer-social-link">
+					<a href="https://www.linkedin.com/in/jameshanks"><svg class="footer-logo"><path d="M17.5,1A16.48,16.48,0,0,1,29.15,29.15,16.48,16.48,0,1,1,5.85,5.85,16.38,16.38,0,0,1,17.5,1m0-1h0A17.51,17.51,0,0,0,0,17.5H0A17.51,17.51,0,0,0,17.5,35h0A17.51,17.51,0,0,0,35,17.5h0A17.51,17.51,0,0,0,17.5,0Z"/><path d="M25.59,19.25v6H22.12V19.65c0-1.4-.5-2.36-1.75-2.36a1.9,1.9,0,0,0-1.78,1.27,2.16,2.16,0,0,0-.12.84v5.83H15s.05-9.45,0-10.43h3.47v1.48l0,0h0v0a3.44,3.44,0,0,1,3.13-1.72c2.28,0,4,1.49,4,4.69ZM11.37,9.77a1.81,1.81,0,1,0,0,3.6h0a1.81,1.81,0,1,0,0-3.6ZM9.62,25.23h3.46V14.8H9.62V25.23Z"/></svg></a>
+				</span>
+				<span class="footer-social-link">
+					<a href="https://www.instagram.com/jameshanks_"><svg class="footer-logo"><path d="M17.5,1A16.48,16.48,0,0,1,29.15,29.15,16.48,16.48,0,1,1,5.85,5.85,16.38,16.38,0,0,1,17.5,1m0-1h0A17.51,17.51,0,0,0,0,17.5H0A17.51,17.51,0,0,0,17.5,35h0A17.51,17.51,0,0,0,35,17.5h0A17.51,17.51,0,0,0,17.5,0Z"/><path d="M17.29,10.45c2.34,0,2.62,0,3.54.05a4.81,4.81,0,0,1,1.63.31,2.86,2.86,0,0,1,1.66,1.66,4.81,4.81,0,0,1,.31,1.63c0,.92.05,1.2.05,3.54s0,2.62-.05,3.55a4.87,4.87,0,0,1-.31,1.63,2.89,2.89,0,0,1-1.66,1.66,4.88,4.88,0,0,1-1.63.3c-.92,0-1.2,0-3.54,0s-2.62,0-3.55,0a4.81,4.81,0,0,1-1.62-.3,2.93,2.93,0,0,1-1.67-1.66,4.94,4.94,0,0,1-.3-1.63c0-.93-.05-1.2-.05-3.55s0-2.62.05-3.54a4.88,4.88,0,0,1,.3-1.63,2.91,2.91,0,0,1,1.67-1.66,4.74,4.74,0,0,1,1.62-.31c.93,0,1.2-.05,3.55-.05m0-1.58c-2.38,0-2.68,0-3.62.05a6.86,6.86,0,0,0-2.13.41A4.47,4.47,0,0,0,9,11.9,6.27,6.27,0,0,0,8.57,14c0,.93-.05,1.23-.05,3.61s0,2.68.05,3.62A6.27,6.27,0,0,0,9,23.39,4.47,4.47,0,0,0,11.54,26a6.61,6.61,0,0,0,2.13.4c.93.05,1.24.06,3.62.06s2.68,0,3.61-.06A6.5,6.5,0,0,0,23,26a4.52,4.52,0,0,0,2.57-2.57A6.52,6.52,0,0,0,26,21.26c0-.93,0-1.23,0-3.61s0-2.68,0-3.62a6.52,6.52,0,0,0-.41-2.13A4.47,4.47,0,0,0,23,9.34a6.22,6.22,0,0,0-2.13-.41c-.93,0-1.23,0-3.61,0"/><path d="M17.29,13.14a4.51,4.51,0,1,0,4.5,4.5h0a4.5,4.5,0,0,0-4.5-4.5m0,7.43a2.93,2.93,0,1,1,2.92-2.93,2.93,2.93,0,0,1-2.92,2.93h0"/><path d="M23,13A1.05,1.05,0,1,1,22,11.91,1,1,0,0,1,23,13h0"/></svg></a>
+				</span>
+				<span class="footer-social-link">
+					<a href="https://www.twitter.com/jameshanks_"><svg class="footer-logo"><path d="M17.5,1A16.48,16.48,0,0,1,29.15,29.15,16.48,16.48,0,1,1,5.85,5.85,16.38,16.38,0,0,1,17.5,1m0-1h0A17.51,17.51,0,0,0,0,17.5H0A17.51,17.51,0,0,0,17.5,35h0A17.51,17.51,0,0,0,35,17.5h0A17.51,17.51,0,0,0,17.5,0Z"/> <path d="M27,12.94A6.25,6.25,0,0,1,26.1,14a6.88,6.88,0,0,1-.91.79.27.27,0,0,0-.12.25,11,11,0,0,1-.47,3.55,11.43,11.43,0,0,1-3.21,5.14,10.37,10.37,0,0,1-5.1,2.57,11.51,11.51,0,0,1-2.93.2A10.92,10.92,0,0,1,9.2,25.49,10.54,10.54,0,0,1,8,24.82a8,8,0,0,0,5.7-1.62,5.85,5.85,0,0,1-.87-.14,3.89,3.89,0,0,1-2.72-2.5c-.05-.13,0-.09.1-.08a3.86,3.86,0,0,0,1.55-.07c0-.06-.12-.05-.18-.07a3.88,3.88,0,0,1-2.81-3.09,5.42,5.42,0,0,1-.06-.58c0-.15,0-.14.16-.09a3.53,3.53,0,0,0,1.53.41c0-.06,0-.06-.07-.09a3.88,3.88,0,0,1-1.56-2.71,3.93,3.93,0,0,1,.43-2.28c0-.09.07-.15.17,0a11.05,11.05,0,0,0,4.78,3.35,10.52,10.52,0,0,0,2.82.6c.11,0,.25.08.32,0s0-.19,0-.29a3.91,3.91,0,0,1,6.62-3.35.27.27,0,0,0,.28.07,7.78,7.78,0,0,0,2.19-.85l.12-.05a4,4,0,0,1-1.61,2.1.3.3,0,0,0,.21,0A8.09,8.09,0,0,0,26.85,13a.27.27,0,0,1,.12,0Z"/></svg></a>
+				</span>
+			</div>
+		</div>
 	</footer>
 </body>
 </html>
