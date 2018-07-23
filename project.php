8e0e38e999880667606c8a765afa7dcfd732af69
JamesHanks <james.ra.hanks@gmail.com>
2018-07-23 21:52:35 +0100

    Case study hero 'parallax'

--- project.php
+++ project.php
@@ -4,6 +4,8 @@
 
 	<!-- Case Study Header -->
 	<section class="case-study-intro" id="intro">
+
+		<!-- Navigation -->
 		<div class="case-study-exit">
 			<a href="index.php"><span>Exit</span></a>
 		</div>
@@ -13,19 +15,23 @@
 			<span><a href="index.php">Work</a></span>
 		</nav>
 
-		<!-- Case Study Intro -->
-		<div class="case-study-intro-image espia"></div>
+		<!-- Intro -->
 		<div class="case-study-intro-text">
 			<h1>Espia</h1>
 			<span class="divider"></span>
 			<h2>Ubiquitous Robot Concept</h2>
 		</div>
 
-		<div class="case-study-intro-overlay"></div>
+		<!-- Hero Image -->
+		<div class="case-study-hero">
+			<div class="case-study-intro-overlay"></div>
+			<div class="case-study-intro-image espia"></div>
+		</div>
+
 	</section>
 
 	<!-- Case Study Content -->
-	<section class="content">
+	<div class="case-study-shade">
 		<div class="case-study-container">
 
 			<div class="case-study-text-wrap">
@@ -88,60 +94,59 @@
 
 			</div>
 		</div>
-	</section>
+	</div>
 
 	<?php include 'includes/footer_1.php'; ?>
 
-		<!-- Next, To-Top and Previous arrows -->
+	<!-- Next, To-Top and Previous arrows -->
 
-		<div class="case-study-arrows-container">
+	<div class="case-study-arrows-container">
 
-			<a href="#">
-				<div class="case-study-arrow-1">
-					<span class="arrow-left-container">
-						<svg class="arrow-left"  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
-							<path d="M50,2.9c26-0.1,47.2,20.9,47.3,47c0.1,12.6-5,24.7-13.9,33.6c-18.5,18.5-48.4,18.5-66.9,0s-18.5-48.4,0-66.9C25.5,7.8,37.5,2.9,50,2.9M50,0L50,0C22.4,0,0,22.4,0,50l0,0c0,27.6,22.4,50,50,50l0,0c27.6,0,50-22.4,50-50l0,0C100,22.4,77.6,0,50,0z"/>
-							<path d="M81.1,52.9H21.7c-1.6,0-2.9-1.3-2.9-2.9c0-1.6,1.3-2.9,2.9-2.9h59.4c1.6,0,2.9,1.3,2.9,2.9C84,51.6,82.7,52.9,81.1,52.9z"/>
-							<path d="M41.7,74.3c-0.7,0-1.5-0.3-2-0.9L16.9,52c-1.1-1.1-1.1-2.9,0-4l22.9-21.4c0.9-1.3,2.7-1.5,4-0.6c1.3,0.9,1.5,2.7,0.6,4c-0.2,0.2-0.4,0.4-0.6,0.6L23.1,50l20.6,19.4c1.1,1.1,1.1,2.9,0,4C43.2,74,42.5,74.3,41.7,74.3z"/>
-						</svg>
-					</span>
-					<span class="arrow-left-text">
-						<h3>Hello Lamp Post</h3>
-						<h4>Previous Project</h4>
-					</span>
-				</div>
-			</a>
+		<a href="#">
+			<div class="case-study-arrow-1">
+				<span class="arrow-left-container">
+					<svg class="arrow-left"  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+						<path d="M50,2.9c26-0.1,47.2,20.9,47.3,47c0.1,12.6-5,24.7-13.9,33.6c-18.5,18.5-48.4,18.5-66.9,0s-18.5-48.4,0-66.9C25.5,7.8,37.5,2.9,50,2.9M50,0L50,0C22.4,0,0,22.4,0,50l0,0c0,27.6,22.4,50,50,50l0,0c27.6,0,50-22.4,50-50l0,0C100,22.4,77.6,0,50,0z"/>
+						<path d="M81.1,52.9H21.7c-1.6,0-2.9-1.3-2.9-2.9c0-1.6,1.3-2.9,2.9-2.9h59.4c1.6,0,2.9,1.3,2.9,2.9C84,51.6,82.7,52.9,81.1,52.9z"/>
+						<path d="M41.7,74.3c-0.7,0-1.5-0.3-2-0.9L16.9,52c-1.1-1.1-1.1-2.9,0-4l22.9-21.4c0.9-1.3,2.7-1.5,4-0.6c1.3,0.9,1.5,2.7,0.6,4c-0.2,0.2-0.4,0.4-0.6,0.6L23.1,50l20.6,19.4c1.1,1.1,1.1,2.9,0,4C43.2,74,42.5,74.3,41.7,74.3z"/>
+					</svg>
+				</span>
+				<span class="arrow-left-text">
+					<h3>Hello Lamp Post</h3>
+					<h4>Previous Project</h4>
+				</span>
+			</div>
+		</a>
 
-			<a href="#" onclick="scrollDown(document.getElementById('intro'))">
-				<div class="case-study-arrow-top">
-					<span class="arrow-top-container">
-						<svg class="arrow-top" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
-							<title>Arrow Top</title>
-							<path class="a" d="M2.9,50A47.1,47.1,0,0,1,83.3,16.7,47.1,47.1,0,1,1,16.7,83.3,46.7,46.7,0,0,1,2.9,50M0,50H0a50,50,0,0,0,50,50h0a50,50,0,0,0,50-50h0A50,50,0,0,0,50,0h0A50,50,0,0,0,0,50Z"/>
-							<path class="a" d="M52.9,21.7V81.1a2.9,2.9,0,0,1-5.8,0V21.7a2.9,2.9,0,0,1,5.8,0Z"/>
-							<path class="a" d="M74.3,41.7a2.9,2.9,0,0,1-.9,2.1,3,3,0,0,1-4.1-.1L50,23,30.7,43.7a3,3,0,0,1-4.1.1,2.8,2.8,0,0,1-.1-4L47.9,16.9a2.9,2.9,0,0,1,4.2,0L73.5,39.8A2.7,2.7,0,0,1,74.3,41.7Z"/>
-						</svg>
-					</span>
-					<h4>Back to Top</h4>
-				</div>
-			</a>
+		<a href="#" onclick="scrollDown(document.getElementById('intro'))">
+			<div class="case-study-arrow-top">
+				<span class="arrow-top-container">
+					<svg class="arrow-top" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+						<title>Arrow Top</title>
+						<path class="a" d="M2.9,50A47.1,47.1,0,0,1,83.3,16.7,47.1,47.1,0,1,1,16.7,83.3,46.7,46.7,0,0,1,2.9,50M0,50H0a50,50,0,0,0,50,50h0a50,50,0,0,0,50-50h0A50,50,0,0,0,50,0h0A50,50,0,0,0,0,50Z"/>
+						<path class="a" d="M52.9,21.7V81.1a2.9,2.9,0,0,1-5.8,0V21.7a2.9,2.9,0,0,1,5.8,0Z"/>
+						<path class="a" d="M74.3,41.7a2.9,2.9,0,0,1-.9,2.1,3,3,0,0,1-4.1-.1L50,23,30.7,43.7a3,3,0,0,1-4.1.1,2.8,2.8,0,0,1-.1-4L47.9,16.9a2.9,2.9,0,0,1,4.2,0L73.5,39.8A2.7,2.7,0,0,1,74.3,41.7Z"/>
+					</svg>
+				</span>
+				<h4>Back to Top</h4>
+			</div>
+		</a>
 
-			<a href="#">
-				<div class="case-study-arrow-2">
-					<span class="arrow-right-container">
-						<svg class="arrow-right" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
-							<path d="M50,2.9c26-0.1,47.2,20.9,47.3,47c0.1,12.6-5,24.7-13.9,33.6C64.9,102,35,102,16.5,83.5s-18.5-48.4,0-66.9C25.5,7.8,37.5,2.9,50,2.9M50,0L50,0C22.4,0,0,22.4,0,50l0,0c0,27.6,22.4,50,50,50l0,0c27.6,0,50-22.4,50-50l0,0C100,22.4,77.6,0,50,0z"/>
-							<path d="M19,46.8h59.4c1.6,0,2.9,1.3,2.9,2.9c0,1.6-1.3,2.9-2.9,2.9H19c-1.6,0-2.9-1.3-2.9-2.9C16.1,48.1,17.4,46.8,19,46.8z"/>
-							<path d="M58.4,25.4c0.7,0,1.5,0.3,2,0.9l22.8,21.4c1.1,1.1,1.1,2.9,0,4L60.3,73.1c-0.9,1.3-2.7,1.5-4,0.6c-1.3-0.9-1.5-2.7-0.6-4c0.2-0.2,0.4-0.4,0.6-0.6L77,49.7L56.4,30.3c-1.1-1.1-1.1-2.9,0-4C56.9,25.7,57.6,25.4,58.4,25.4z"/>
-						</svg>
-					</span>
-					<span class="arrow-right-text">
-						<h3>Apto Hotels</h3>
-						<h4>Next Project</h4>
-					</span>
-				</div>
-			</a>
+		<a href="#">
+			<div class="case-study-arrow-2">
+				<span class="arrow-right-container">
+					<svg class="arrow-right" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+						<path d="M50,2.9c26-0.1,47.2,20.9,47.3,47c0.1,12.6-5,24.7-13.9,33.6C64.9,102,35,102,16.5,83.5s-18.5-48.4,0-66.9C25.5,7.8,37.5,2.9,50,2.9M50,0L50,0C22.4,0,0,22.4,0,50l0,0c0,27.6,22.4,50,50,50l0,0c27.6,0,50-22.4,50-50l0,0C100,22.4,77.6,0,50,0z"/>
+						<path d="M19,46.8h59.4c1.6,0,2.9,1.3,2.9,2.9c0,1.6-1.3,2.9-2.9,2.9H19c-1.6,0-2.9-1.3-2.9-2.9C16.1,48.1,17.4,46.8,19,46.8z"/>
+						<path d="M58.4,25.4c0.7,0,1.5,0.3,2,0.9l22.8,21.4c1.1,1.1,1.1,2.9,0,4L60.3,73.1c-0.9,1.3-2.7,1.5-4,0.6c-1.3-0.9-1.5-2.7-0.6-4c0.2-0.2,0.4-0.4,0.6-0.6L77,49.7L56.4,30.3c-1.1-1.1-1.1-2.9,0-4C56.9,25.7,57.6,25.4,58.4,25.4z"/>
+					</svg>
+				</span>
+				<span class="arrow-right-text">
+					<h3>Apto Hotels</h3>
+					<h4>Next Project</h4>
+				</span>
+			</div>
+		</a>
+	</div>
 
-		</div>
-
-<?php include 'includes/footer_2.php'; ?>
+	<?php include 'includes/footer_2.php'; ?>
